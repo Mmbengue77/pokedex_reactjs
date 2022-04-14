174320831bb3df81018ad682e2889603998f831b
Cynthia <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2022-04-14 22:22:34 +0200

    ListePokemon: amélioration code pour les types/faiblesses (avec image)

--- src/ListePokemon.js
+++ src/ListePokemon.js
@@ -22,6 +22,93 @@ export default function ListePokemon() {
 
     }, [])
 
+    // Récupérer l'URL de l'image du type
+    const getTypeImage = (type) => {
+        const {
+            REACT_APP_TYPE_GRASS,
+            REACT_APP_TYPE_FIRE,
+            REACT_APP_TYPE_WATER,
+            REACT_APP_TYPE_LIGHTNING,
+            REACT_APP_TYPE_FIGHTING,
+            REACT_APP_TYPE_PSYCHIC,
+            REACT_APP_TYPE_COLORLESS,
+            REACT_APP_TYPE_DARKNESS,
+            REACT_APP_TYPE_METAL,
+            REACT_APP_TYPE_DRAGON,
+            REACT_APP_TYPE_FAIRY
+        } = process.env
+
+        const typeToLowerCase = type.toLowerCase()
+
+        switch(typeToLowerCase) {
+            case ['grass','bug','poison'].find(t => t === typeToLowerCase):
+                return REACT_APP_TYPE_GRASS
+            
+            case 'fire':
+                return REACT_APP_TYPE_FIRE
+
+            case ['water','ice'].find(t => t === typeToLowerCase):
+                return REACT_APP_TYPE_WATER
+
+            case 'electric':
+                return REACT_APP_TYPE_LIGHTNING
+
+            case ['fighting','rock', 'ground'].find(t => t === typeToLowerCase):
+                return REACT_APP_TYPE_FIGHTING
+
+            case ['psychic','ghost', 'poison', 'fairy'].find(t => t === typeToLowerCase):
+                return REACT_APP_TYPE_PSYCHIC
+
+            case ['normal','flying', 'dragon'].find(t => t === typeToLowerCase):
+                return REACT_APP_TYPE_COLORLESS
+
+            case ['dark','poison'].find(t => t === typeToLowerCase):
+                return REACT_APP_TYPE_DARKNESS
+
+            case 'steel':
+                return REACT_APP_TYPE_METAL
+
+            case 'dragon':
+                return REACT_APP_TYPE_DRAGON
+
+            case 'fairy':
+                return REACT_APP_TYPE_FAIRY
+
+            default:
+                return false
+
+        }
+    }
+
+    // Afficher les images selon les types
+    const displayType = (types) => {
+        // S'il n'y a qu'un seul type à afficher
+        if(types.length === 1) {
+            return(
+                <div className='Pokemon-type' title={types[0]}>
+                    <img src={getTypeImage(types[0])} alt={types[0]} />
+                </div>
+            )
+        } else { // S'il y en a plusieurs
+
+            // Convertir l'array en string délimité par une virgule
+            const typesName = types.join(', ')
+
+            // Récupérer toutes les URL
+            const typesURL = types.map( type => getTypeImage(type))
+
+            // Supprimer les doublons
+            const uniqueTypesURL = typesURL.filter( (type,i,typesURL) => typesURL.indexOf(type) === i )
+
+            return(
+                <div className='Pokemon-type' title={typesName}>
+                    { uniqueTypesURL.map( (url,i) => <img key={types[i]} src={url} alt={types[i]} />) }
+                </div>
+            )
+
+        }
+    }
+
     return(
         <div className='ListePokemon'>
             <Link to="/ajouter-un-pokemon">
@@ -35,13 +122,8 @@ export default function ListePokemon() {
                                 <h3>N°{p.num} - {p.name}</h3>
                                 <img src={p.img} alt={p.name} />    
                                 <ul>
-                                    <li>TYPE : {p.type.length > 1 ? p.type[0] + " / " + p.type[1] : p.type[0]}</li>
-                                    <li>
-                                        FAIBLESSE : 
-                                        {
-                                            p.weaknesses.length === 1 ? p.weaknesses[0] : p.weaknesses.map( (w, i) => i < p.weaknesses.length - 1 ?  " " + w + ", " :  " " + w)
-                                        }
-                                    </li>
+                                    <li className='Pokemon-li_type'>TYPE : { displayType(p.type) }</li>
+                                    <li className='Pokemon-li_weaknesses'>FAIBLESSE : { displayType(p.weaknesses) }</li>
                                 </ul>
                                 <div>
                                     <Link to={"/pokemon/" + p.num}>
